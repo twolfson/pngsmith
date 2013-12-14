47ea6d97fbdad39aa81d26590bcb6a0d8a5f5485
Todd Wolfson <todd@twolfson.com>
2013-12-14 00:32:00 -0800

    Added multiple/repeating image overrides from spritesmith-engine-test

--- test/pngsmith_test_content.js
+++ test/pngsmith_test_content.js
@@ -1,8 +1,13 @@
 var pngsmith = require('../'),
     expect = require('chai').expect,
     extend = require('obj-extend'),
-    commonTest = require('spritesmith-engine-test').content;
-module.exports = extend({}, commonTest, {
+    commonTest = require('spritesmith-engine-test');
+
+// Override images with png variants
+commonTest.config.multipleImages = commonTest.config.multiplePngImages;
+commonTest.config.repeatingImages = commonTest.config.repeatingPngImages;
+
+module.exports = extend({}, commonTest.content, {
   'pngsmith': function () {
     this.smith = pngsmith;
 
