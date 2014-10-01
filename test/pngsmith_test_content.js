5d98fc90e9d15be82378ca3adce3c9c7e3df847b
Todd Wolfson <todd@twolfson.com>
2014-10-01 12:19:07 +0900

    Upgraded `spritesmith-engine-test` and removed local expected images

--- test/pngsmith_test_content.js
+++ test/pngsmith_test_content.js
@@ -10,11 +10,5 @@ commonTest.config.repeatingImage = commonTest.config.repeatingPngImage;
 module.exports = extend({}, commonTest.content, {
   'pngsmith': function () {
     this.smith = pngsmith;
-
-    var expectedDir = __dirname + '/expected_files/';
-    this.expectedFilepaths = [
-      expectedDir + '/multiple-node-0.8.png',
-      expectedDir + '/multiple-node-0.10.png'
-    ];
   }
 });
