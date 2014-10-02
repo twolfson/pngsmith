a95a0485da3274dc16a6bde4b98aa3ef3978dee9
Todd Wolfson <todd@twolfson.com>
2014-10-02 15:39:56 +0900

    Migrated to latest spritesmith-engine-test and mocha

---
+++ test/pngsmith_test.js
@@ -0,0 +1,13 @@
+// Load our dependencies
+var spritesmithEngineTest = require('spritesmith-engine-test');
+var pngsmith = require('../');
+
+// Override images with png variants
+spritesmithEngineTest.config.multipleImages = spritesmithEngineTest.config.multiplePngImages;
+spritesmithEngineTest.config.repeatingImages = spritesmithEngineTest.config.repeatingPngImages;
+
+// Run our test
+spritesmithEngineTest.run({
+  engine: pngsmith,
+  engineName: 'pngsmith'
+});
