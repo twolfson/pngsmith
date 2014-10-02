2167c7d1b643fb4ca1637bcb82bb198bdc23aa27
Todd Wolfson <todd@twolfson.com>
2014-10-02 15:44:27 +0900

    Adding request for future self

--- test/pngsmith_test.js
+++ test/pngsmith_test.js
@@ -3,6 +3,7 @@ var spritesmithEngineTest = require('spritesmith-engine-test');
 var pngsmith = require('../');
 
 // Override images with png variants
+// TODO: Define as a flag inside of `spritesmith-engine-test`
 spritesmithEngineTest.config.multipleImages = spritesmithEngineTest.config.multiplePngImages;
 spritesmithEngineTest.config.repeatingImages = spritesmithEngineTest.config.repeatingPngImages;
 
